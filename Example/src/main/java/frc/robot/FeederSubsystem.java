b08ecf1571e632a17255d515d4e222fcee4f53f6
Adamtheboss9 <adamtheboss78@gmail.com>
2024-11-18 17:18:36 -0500

    Fixed Project

--- Example/src/main/java/frc/robot/FeederSubsystem.java
+++ Example/src/main/java/frc/robot/FeederSubsystem.java
@@ -3,20 +3,42 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot;
+import java.util.function.DoubleSupplier;
 
 import org.frc5010.common.arch.GenericSubsystem;
+import org.frc5010.common.motors.MotorFactory;
+import org.frc5010.common.motors.function.PercentControlMotor;
 
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class FeederSubsystem extends GenericSubsystem {
+
+
+
+PercentControlMotor motor;
+
   /** Creates a new Feeder. */
   public FeederSubsystem(Mechanism2d simulatedRobot) {
     setMechSimulation(simulatedRobot);
+
+    motor = new PercentControlMotor(MotorFactory.NEO(11));
+
+    motor.setVisualizer(mechanismSimulation, new Pose3d(0.05, 1.0, 0.4, new Rotation3d()), "Feeder Neo");
+    motor.setupSimulatedMotor(1.0, 1.0);
+
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    motor.draw();
+
   }
+
+  @Override
+  public void simulationPeriodic(){
+    motor.simulationUpdate();
+  }
+  public void runMotor(DoubleSupplier speed){}
 }
