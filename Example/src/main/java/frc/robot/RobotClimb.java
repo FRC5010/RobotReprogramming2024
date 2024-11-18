5f92681322a0ea42a44ed61211fba61262bd1812
samstrawhacker <strawhsj@hotmail.com>
2024-11-18 14:57:57 -0800

    added current switches

--- Example/src/main/java/frc/robot/RobotClimb.java
+++ Example/src/main/java/frc/robot/RobotClimb.java
@@ -5,16 +5,63 @@
 package frc.robot;
 
 import org.frc5010.common.arch.GenericSubsystem;
+import org.frc5010.common.motors.function.PercentControlMotor;
+import org.frc5010.common.sensors.ValueSwitch;
+import org.frc5010.common.sensors.encoder.GenericEncoder;
 
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class RobotClimb extends GenericSubsystem {
-  PercentControlMotor motor
+  PercentControlMotor leftClimbMotor;
+  PercentControlMotor rightClimbMotor;
+  GenericEncoder rightEncoder;
+  GenericEncoder leftEncoder;
+  ValueSwitch leftCurrentSwitch;
+  ValueSwitch rightCurrentSwitch;
   /** Creates a new RobotClimb. */
-  public RobotClimb(Mechanism2d simulatedRobot) {
+  public RobotClimb(Mechanism2d simulatedRobot, PercentControlMotor leftMotor, PercentControlMotor rightMotor) {
       setMechSimulation(simulatedRobot);
+      leftClimbMotor = leftMotor;
+      rightClimbMotor = rightMotor;
+      leftEncoder = leftMotor.getMotorEncoder();
+      rightEncoder = rightMotor.getMotorEncoder();
+      leftEncoder.setPositionConversion (1.0);
+      rightEncoder.setPositionConversion (1.0);
+      leftCurrentSwitch = new ValueSwitch(40.0, ()->0.0, 1.0);
+      leftCurrentSwitch = new ValueSwitch(40.0, ()->0.0, 1.0);
   }
+
+public void setSpeedRight (double speedRight) {
+  rightClimbMotor.set(speedRight);
+}
+public void setSpeedLeft (double speedLeftt) {
+  leftClimbMotor.set(speedLeftt);
+}
+
+public double getLeftPosition () {
+  return leftEncoder.getPosition();
+}
+
+public double getRightPosition () {
+  return rightEncoder.getPosition();
+}
+
+public boolean leftIsAtMin () {
+  return false;
+}
+
+public boolean rightIsAtMin () {
+  return false;
+}
+
+public boolean leftIsAtMax () {
+  return false;
+}
+
+public boolean rightIsAtMax () {
+  return false;
+}
 
   @Override
   public void periodic() {
--- Example/src/main/java/frc/robot/commands/BasicRunClimb.java
+++ Example/src/main/java/frc/robot/commands/BasicRunClimb.java
@@ -4,25 +4,45 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj2.command.Command;
+import java.util.function.DoubleSupplier;
 
-public class BasicRunClimb extends Command {
+import org.frc5010.common.arch.GenericCommand;
+import org.frc5010.common.sensors.camera.GenericCamera;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.RobotClimb;
+
+public class BasicRunClimb extends GenericCommand {
   /** Creates a new BasicRunClimb. */
-  public BasicRunClimb() {
+  RobotClimb climbSubsystem;
+  DoubleSupplier leftSpeed;
+  DoubleSupplier rightSpeed;
+  public BasicRunClimb(RobotClimb climbSubsystem, DoubleSupplier rightSpeed, DoubleSupplier leftSpeed) {
     // Use addRequirements() here to declare subsystem dependencies.
+    this.climbSubsystem = climbSubsystem;
+    this.rightSpeed = rightSpeed;
+    this.leftSpeed = leftSpeed;
+    addRequirements(climbSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void init() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    climbSubsystem.setSpeedLeft (leftSpeed.getAsDouble());
+    climbSubsystem.setSpeedRight (rightSpeed.getAsDouble());
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void stop (boolean interrupted) {
+    climbSubsystem.setSpeedLeft (0);
+    climbSubsystem.setSpeedRight (0);
+  }
+  
 
   // Returns true when the command should end.
   @Override
