b5c59b68ce7da7d68d2f1eac1999f45900c1a518
samstrawhacker <strawhsj@hotmail.com>
2024-11-25 14:41:43 -0800

    Set zeroing, minimum, maximum for climb motors

--- Example/src/main/java/frc/robot/RobotClimb.java
+++ Example/src/main/java/frc/robot/RobotClimb.java
@@ -4,13 +4,21 @@
 
 package frc.robot;
 
+import java.util.function.DoubleSupplier;
+import java.util.function.Supplier;
+
 import org.frc5010.common.arch.GenericSubsystem;
 import org.frc5010.common.motors.function.PercentControlMotor;
 import org.frc5010.common.sensors.ValueSwitch;
 import org.frc5010.common.sensors.encoder.GenericEncoder;
 
+import com.revrobotics.CANSparkMax;
+
 import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
 
 public class RobotClimb extends GenericSubsystem {
   PercentControlMotor leftClimbMotor;
@@ -19,6 +27,8 @@ public class RobotClimb extends GenericSubsystem {
   GenericEncoder leftEncoder;
   ValueSwitch leftCurrentSwitch;
   ValueSwitch rightCurrentSwitch;
+  private final double maxHeight = 100;
+  private final double minMaxThreshold = 1;
   /** Creates a new RobotClimb. */
   public RobotClimb(Mechanism2d simulatedRobot, PercentControlMotor leftMotor, PercentControlMotor rightMotor) {
       setMechSimulation(simulatedRobot);
@@ -28,8 +38,10 @@ public class RobotClimb extends GenericSubsystem {
       rightEncoder = rightMotor.getMotorEncoder();
       leftEncoder.setPositionConversion (1.0);
       rightEncoder.setPositionConversion (1.0);
-      leftCurrentSwitch = new ValueSwitch(40.0, ()->0.0, 1.0);
-      leftCurrentSwitch = new ValueSwitch(40.0, ()->0.0, 1.0);
+      Supplier<Double> leftCurrent = ()-> ((CANSparkMax)leftClimbMotor.getMotor()).getOutputCurrent();
+      Supplier<Double> rightCurrent = ()-> ((CANSparkMax)rightClimbMotor.getMotor()).getOutputCurrent();
+      leftCurrentSwitch = new ValueSwitch(40.0, leftCurrent, 1.0);
+      rightCurrentSwitch = new ValueSwitch(40.0, rightCurrent, 1.0);
   }
 
 public void setSpeedRight (double speedRight) {
@@ -47,21 +59,56 @@ public double getRightPosition () {
   return rightEncoder.getPosition();
 }
 
+public Trigger leftCurrentSwitchIsHit () {
+  return new Trigger(()-> leftCurrentSwitch.get());
+}
+
+public Trigger rightCurrentSwitchIsHit () {
+  return new Trigger(()-> rightCurrentSwitch.get());
+}
+
 public boolean leftIsAtMin () {
-  return false;
+  return getLeftPosition()<minMaxThreshold;
 }
 
 public boolean rightIsAtMin () {
-  return false;
+  return getRightPosition()<minMaxThreshold;
 }
 
 public boolean leftIsAtMax () {
-  return false;
+  return getRightPosition()>(maxHeight-minMaxThreshold);
 }
 
 public boolean rightIsAtMax () {
-  return false;
+  return getRightPosition()>(maxHeight-minMaxThreshold);
 }
+
+public Command zeroClimb () {
+  return Commands.parallel (zeroLeftClimb(), zeroRightClimb());
+}
+
+private void resetLeftEncoder () {
+  leftEncoder.reset ();
+}
+
+private void resetRightEncoder () {
+  rightEncoder.reset ();
+}
+
+public Command zeroLeftClimb () {
+  return Commands.run(()-> setSpeedLeft(0.5), this)
+    .until(leftCurrentSwitchIsHit())
+    .andThen(Commands.runOnce(this::resetLeftEncoder));
+    
+
+}
+
+public Command zeroRightClimb () {
+  return Commands.run(()-> setSpeedRight(0.5), this)
+    .until(rightCurrentSwitchIsHit())
+    .andThen(Commands.runOnce(this::resetRightEncoder));
+}
+
 
   @Override
   public void periodic() {
