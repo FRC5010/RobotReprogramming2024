60c7b9f074ceed2e960635300b791e8cdc45b59a
Adamtheboss9 <adamtheboss78@gmail.com>
2024-12-02 16:15:08 -0500

    Feeder Additions

--- Example/src/main/java/frc/robot/ReprogramRobot.java
+++ Example/src/main/java/frc/robot/ReprogramRobot.java
@@ -12,24 +12,37 @@ import org.frc5010.common.drive.GenericDrivetrain;
 import org.frc5010.common.sensors.Controller;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 // Reference Document Link: https://docs.google.com/document/d/16YqkkYDurYPHd9RxBJdwxWiMCAomJKIatphhviYabxw/edit?usp=sharing
 /** This is an example robot class. */
 public class ReprogramRobot extends GenericRobot {
   SwerveConstants swerveConstants;
   GenericDrivetrain drivetrain;
+  FeederSubsystem feeder;
 
   public ReprogramRobot(String directory) {
     super(directory);
     drivetrain = (GenericDrivetrain) getSubsystem(ConfigConstants.DRIVETRAIN);
+    feeder = new FeederSubsystem(mechVisual);
   }
 
   @Override
   public void configureButtonBindings(Controller driver, Controller operator) {
+    driver.createAButton()
+    .and(feeder.atStop.negate()
+    .and(feeder.detected.negate()).and(isIdle))
+    .onTrue(feeder.runFeeder(()->0.5));
+    driver.createAButton()
+    .or(feeder.atStop
+    .or(feeder.detected))
+    .onTrue(getAutonom)
   }
+
 
   @Override
   public void setupDefaultCommands(Controller driver, Controller operator) {
     drivetrain.setDefaultCommand(drivetrain.createDefaultCommand(driver));
+    feeder.setDefaultCommand(Commands.run(() -> feeder.runMotor(()->driver.getLeftTrigger()), feeder));
   }
 
   @Override
