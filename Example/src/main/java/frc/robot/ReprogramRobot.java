087498cf97a3434cb06140389ac31e8e16366ea2
5010 Tiger Dynasty <roboticsteam.fhs@gmail.com>
2024-12-02 16:55:44 -0500

    Update Main

--- Example/src/main/java/frc/robot/ReprogramRobot.java
+++ Example/src/main/java/frc/robot/ReprogramRobot.java
@@ -9,28 +9,44 @@ import org.frc5010.common.config.ConfigConstants;
 import org.frc5010.common.config.json.DrivetrainPropertiesJson;
 import org.frc5010.common.constants.SwerveConstants;
 import org.frc5010.common.drive.GenericDrivetrain;
+import org.frc5010.common.motors.MotorFactory;
+import org.frc5010.common.motors.function.PercentControlMotor;
 import org.frc5010.common.sensors.Controller;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.BasicRunClimb;
 // Reference Document Link: https://docs.google.com/document/d/16YqkkYDurYPHd9RxBJdwxWiMCAomJKIatphhviYabxw/edit?usp=sharing
 /** This is an example robot class. */
 public class ReprogramRobot extends GenericRobot {
   SwerveConstants swerveConstants;
   GenericDrivetrain drivetrain;
 
+  private RobotClimb climbSubsystem;
+	private PercentControlMotor climbMotorLeft;
+	private PercentControlMotor climbMotorRight;
+
   public ReprogramRobot(String directory) {
     super(directory);
+    climbMotorLeft = new PercentControlMotor(MotorFactory.NEO(7).invert(true));
+    climbMotorRight = new PercentControlMotor(MotorFactory.NEO(8));
+    climbSubsystem = new RobotClimb(mechVisual, climbMotorLeft, climbMotorRight);
+
     drivetrain = (GenericDrivetrain) getSubsystem(ConfigConstants.DRIVETRAIN);
   }
 
   @Override
   public void configureButtonBindings(Controller driver, Controller operator) {
+    operator.createBButton ().onTrue(climbSubsystem.zeroClimb());
   }
 
   @Override
   public void setupDefaultCommands(Controller driver, Controller operator) {
     drivetrain.setDefaultCommand(drivetrain.createDefaultCommand(driver));
-  }
+    climbSubsystem.setDefaultCommand(new BasicRunClimb(climbSubsystem, ()-> operator.getRightYAxis(), ()-> operator.getLeftYAxis())); 
+
+
+    }
+  
 
   @Override
   public void initAutoCommands() {
